5e4d3e444c8c22bb79bea2a6a5071c182034c02e
unknown <sergeyandmelnikov@gmail.com>
2022-02-03 00:19:41 +0300

    new api version and first fix point

---
+++ src/app/Http/Controllers/Api/v2/LocalityController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api\v2;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\Locality;
+use App\Http\Resources\LocalityResource;
+use App\Http\Requests\LocalityStoreRequest;
+
+class LocalityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+	return LocalityResource::collection(Locality::with('years')->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(LocalityStoreRequest $request)
+    {
+	$location = Locality::create($request->validated());
+
+	return new LocalityResource($location);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return new LocalityResource(Locality::with('years')->findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(LocalityStoreRequest $request, Locality $location)
+    {
+	$location->update($request->validated());
+
+	return new LocalityResource($location);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Locality $location)
+    {
+	$location->delete();
+
+	return responce(null, Responce::HTTP_NO_CONTENT);
+
+    }
+}
